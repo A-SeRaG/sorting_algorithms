9085ba9fec15ea82145834437c875743e8e623b1
SeRaG <aserag0101@gmail.com>
2024-02-17 07:55:00 -0800

    programming

--- sort.h
+++ sort.h
@@ -26,6 +26,9 @@ void print_list(const listint_t *list);
 
 /*prototype function*/
 void bubble_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif
 
