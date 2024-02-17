eabaf5f8ac06706e0ce29b29c7099e656b6265cf
tamerkat <mahad10taha@gmail.com>
2024-02-17 19:22:38 +0200

    task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,54 @@
+#include "sort.h"
+
+
+/**
+ * swap - Entry point
+ * @x: node one
+ * @y: node two
+ * Return: 0 on success, -1 on failure
+*/
+
+void swap(listint_t *x, listint_t *y)
+{
+	if (!x->prev)
+		x->prev->next = y;
+	if (!y->next)
+		y->next->prev = x;
+	x->prev = y;
+	y->next = x;
+	x->next = y->next;
+	x->prev = y->prev;
+}
+
+/**
+ * insertion_sort_list - Entry point
+ * @list: douple pointer
+ * Return: always succuss
+*/
+void insertion_sort_list(listint_t **list)
+{
+	int i, j;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+	i = (*list)->next;
+	while (i)
+	{
+		j = i;
+		i = i->next;
+
+		while (j && j->prev)
+		{
+			if (j->prev > j)
+			{
+				swap(j->prev, j);
+				if (j->prev)
+					*list = j;
+				print_list((const listint_t *)*list)
+			}
+			else
+				j = j->prev;
+		}
+	}
+}
+
