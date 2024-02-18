bfd48abad677dc187164735e83ad9912a9ef9b59
tamerkat <mahad10taha@gmail.com>
2024-02-18 15:02:16 +0200

    tasks 0

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * swap - Entry point
+ * swaping - Entry point
  * @x: node one
  * @y: node two
  * Return: 0 on success, -1 on failure
@@ -9,46 +9,46 @@
 
 void swaping(listint_t *x, listint_t *y)
 {
-    if (!x->prev)
-        x->prev->next = y;
-    if (!y->next)
-        y->next->prev = x;
-    x->prev = y;
-    y->next = x;
-    x->next = y->next;
-    x->prev = y->prev;
+	if (!x->prev)
+	x->prev->next = y;
+	if (!y->next)
+	y->next->prev = x;
+	x->prev = y;
+	y->next = x;
+	x->next = y->next;
+	x->prev = y->prev;
 }
 
 /**
  * insertion_sort_list - Entry point
  * @list: douple pointer
- * @Return: always succuss
+ * Return: always succuss
 */
 void insertion_sort_list(listint_t **list)
 {
-    int i, j;
+	int i, j;
 
-    if (!list || !*list || !(*list)->next)
-        return;
-    i = (*list)->next;
-    while(i)
-    {
-        j = i;
-        i = i->next;
+	if (!list || !*list || !(*list)->next)
+	return;
+	i = (*list)->next;
+	while (i)
+	{
+	j = i;
+	i = i->next;
 
-        while (j && j->prev)
-        {
-            if (j->prev > j)
-            {
-                swaping(j->prev, j);
-                if (!j->prev)
-                {
-                    *list = j;
-                }
-                print_list((const listint_t *)*list)
-            }
-            else
-                j = j->prev;
-        }
-    }
+	while (j && j->prev)
+	{
+	if (j->prev > j)
+	{
+	swaping(j->prev, j);
+	if (!j->prev)
+	{
+	*list = j;
+	}
+	print_list((const listint_t *)*list)
+	}
+	else
+	j = j->prev;
+	}
+	}
 }
