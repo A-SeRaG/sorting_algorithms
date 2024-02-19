172b0f01be8281d81f44652a9aa933d59e9a34f5
SeRaG <aserag0101@gmail.com>
2024-02-19 12:09:25 -0800

    programming

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,22 +1,24 @@
 #include "sort.h"
 
 /**
- * swp_node - swap 2 nodes in list
- * @x: node one
- * @y: node two
- * Return: void
-*/
-
-void swp_node(listint_t *x, listint_t *y)
+ * swp_node - Swap two nedes
+ * @h: A pointer to the head of the list.
+ * @n1: A pointer to the first node
+ * @n2: A pointer to The second node
+ */
+void swp_node(listint_t **h, listint_t **n1, listint_t *n2)
 {
-	if (x->prev)
-		x->prev->next = y;
-	if (y->next)
-		y->next->prev = x;
-	x->prev = y;
-	y->next = x;
-	x->next = y->next;
-	x->prev = y->prev;
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+	else
+		*h = n2;
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
 }
 
 /**
@@ -29,17 +31,19 @@ void insertion_sort_list(listint_t **list)
 	listint_t *i, *j, *k;
 
 	if (!list || !*list || !(*list)->next)
-		return;
+			return;
 
-	for (i = (*list)->next; i != NULL; i = i->next)
+	for (i = (*list)->next; i != NULL; i = k)
 	{
 		k = i->next;
 		j = i->prev;
 		while (j != NULL && i->n < j->n)
 		{
-			swp_node(j, i);
+			swp_node(list, &j, i);
 			print_list((const listint_t *)*list);
+			j = i->prev;
 		}
+		if (i->prev == NULL)
+			*list = i;
 	}
 }
-
