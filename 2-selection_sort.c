b46d41f0fdc49512e67dcfdd34e1748d33173f5f
tamerkat <mahad10taha@gmail.com>
2024-02-19 17:15:50 +0200

    tasks 0

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,28 +1,30 @@
 #include "sort.h"
 
 /**
- * swaping - Swap
- * @x: first
- * @y: second
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
  */
-void swaping(int *x, int *y)
+void swap_ints(int *a, int *b)
 {
-	int curunt;
+	int tmp;
 
-	curunt = *x;
-	*x = *y;
-	*y = curunt;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
- * selection_sort - bitc
- * @array: array
- * @size: The
- * Return: suc
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
  */
 void selection_sort(int *array, size_t size)
 {
-	int *nolds;
+	int *min;
 	size_t i, j;
 
 	if (array == NULL || size < 2)
@@ -30,13 +32,13 @@ void selection_sort(int *array, size_t size)
 
 	for (i = 0; i < size - 1; i++)
 	{
-		nolds = array + i;
+		min = array + i;
 		for (j = i + 1; j < size; j++)
-			nolds = (array[j] < *nolds) ? (array + j) : nolds;
+			min = (array[j] < *min) ? (array + j) : min;
 
-		if ((array + i) != nolds)
+		if ((array + i) != min)
 		{
-			swaping(array + i, mnolds);
+			swap_ints(array + i, min);
 			print_array(array, size);
 		}
 	}
